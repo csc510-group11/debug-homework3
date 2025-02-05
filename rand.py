e9dff1355cd2087ecc5228c1b0d0f56f04d06776
Sohom Datta <sdatta4@ncsu.edu>
2025-02-05 05:07:10 +0000

    Fixed code + custom algo

--- rand.py
+++ rand.py
@@ -2,7 +2,8 @@
 This module provides functionality to generate and print a random array.
 """
 import secrets
-import subprocess
+import subprocess  # nosec
+
 
 def random_array_nafreen(arr):
     """
@@ -14,9 +15,11 @@ def random_array_nafreen(arr):
     Returns:
         list: A new list containing the elements of arr in random order.
     """
-    for i,_ in enumerate(arr):
-        arr[i] = secrets.randbelow(20) + 1  # Generate a random number between 1 and 20
+    for i, _ in enumerate(arr):
+        # Generate a random number between 1 and 20
+        arr[i] = secrets.randbelow(20) + 1
     return arr
+
 
 def random_array_mrahma22(arr):
     """
@@ -32,9 +35,14 @@ def random_array_mrahma22(arr):
         arr[i] = secrets.randbelow(20) + 1
     return arr
 
+
 def random_array_sdatta4(arr):
+    """
+    Creates fills a array with randomly shuffled numbers from 1-20
+    """
     shuffled_num = None
-    for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
+    for i in enumerate(arr):
+        shuffled_num = subprocess.run(
+            ["/usr/bin/shuf", "-i1-20", "-n1"], capture_output=True, check=True) #nosec B603
         arr[i] = int(shuffled_num.stdout)
     return arr
